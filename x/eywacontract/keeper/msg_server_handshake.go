67ed263c59bfddb188ca7fc1e34371f63f2417f6
exit <chungjung.d@gmail.com>
2023-09-09 01:32:27 +0900

    👔 Business logic of handshake

--- x/eywacontract/keeper/msg_server_handshake.go
+++ x/eywacontract/keeper/msg_server_handshake.go
@@ -2,16 +2,33 @@ package keeper
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
 
 	"eywa-contract/x/eywacontract/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) Handshake(goCtx context.Context, msg *types.MsgHandshake) (*types.MsgHandshakeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	currentTime := time.Now().Local()
+	var currentTimeBytes = []byte(currentTime.Format("2006-01-02 15:04:05.999"))
+	var IndexBytes = append(currentTimeBytes, append([]byte(msg.Creator), []byte(msg.Receiver)...)...)
+	var IndexHash = sha256.Sum256(IndexBytes)
+	var IndexHashString = hex.EncodeToString(IndexHash[:])
+
+	var handshake = types.HandshakeType{
+		Index:         IndexHashString,
+		Sender:        msg.Creator,
+		Receiver:      msg.Receiver,
+		ServerAddress: msg.ServerAddress,
+		Time:          msg.Time,
+	}
+
+	k.SetHandshakeType(ctx, handshake)
 
 	return &types.MsgHandshakeResponse{}, nil
 }
