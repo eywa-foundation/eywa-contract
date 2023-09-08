a471f63687d0b0a7614eea12af91b10b0f9e20c4
exit <chungjung.d@gmail.com>
2023-09-09 00:36:27 +0900

    👔 Business logic of send_chat message

--- x/eywacontract/keeper/msg_server_send_chat.go
+++ x/eywacontract/keeper/msg_server_send_chat.go
@@ -2,15 +2,33 @@ package keeper
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
 
 	"eywa-contract/x/eywacontract/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) SendChat(goCtx context.Context, msg *types.MsgSendChat) (*types.MsgSendChatResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
+	currentTime := time.Now().Local()
+	var currentTimeBytes = []byte(currentTime.Format("2006-01-02 15:04:05.999"))
+	var IndexBytes = append(currentTimeBytes, append([]byte(msg.Creator), []byte(msg.Receiver)...)...)
+	var IndexHash = sha256.Sum256(IndexBytes)
+	var IndexHashString = hex.EncodeToString(IndexHash[:])
+
+	var chat = types.SendChatType{
+		Index:    IndexHashString,
+		Sender:   msg.Creator,
+		Receiver: msg.Receiver,
+		Chat:     msg.Chat,
+	}
+
+	k.SetSendChatType(ctx, chat)
+
 	_ = ctx
 
 	return &types.MsgSendChatResponse{}, nil
